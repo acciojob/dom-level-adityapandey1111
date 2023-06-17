5fa2c8a5c6db86094328e47ba311f3baf58e4627
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-06-17 20:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const levelElement = document.getElementById("level");
+  let level = 0;
+
+  while (levelElement.parentElement) {
+    levelElement = levelElement.parentElement;
+    level++;
+  }
+
+  alert("The level of the element is: " + level);
+});
